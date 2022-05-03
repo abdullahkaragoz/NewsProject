831aabfa1f384fc753f46cbd04203121ada7fe49
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2022-05-03 18:25:55 +0300

    Create Project
    Add UI

---
+++ NewsProject/NewsProject/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  NewsProject
+//
+//  Created by AbdullahKaragoz on 3.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+   
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsCell
+        
+        return cell
+    }
+
+}
+
